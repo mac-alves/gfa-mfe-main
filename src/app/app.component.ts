b995f82d46b5c73a6165e593b047d0181ed99bc6
Mauricio <maurcioalves@ciandt.com>
2023-01-02 11:35:11 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gfa-mfe-main-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gfa-mfe-main';
+}
